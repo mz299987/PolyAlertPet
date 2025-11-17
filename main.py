bee9e3e527049d5e48fd302e1cb611c132777878
mz299987 <darmione1404@gmail.com>
2025-11-17 16:15:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import os
 import sys
 
 # Добавляем путь к модулям
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'newapp'))
+sys.path.insert(0, os.path.dirname(__file__))
 
 from newapp.bot import main
+
 
 if __name__ == "__main__":
     try:
