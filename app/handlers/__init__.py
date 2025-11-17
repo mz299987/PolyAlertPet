2cc5a17ebf27dd1ffe8e1b3997a06b9efd4bbcd7
mz299987 <darmione1404@gmail.com>
2025-11-17 14:28:45 +0500

    Delete app directory

--- app/handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-def register_handlers():
-    # импорт самих модулей достаточно, чтобы декораторы отработали
-    from . import start  # noqa: F401
-    from . import wallets  # noqa: F401
-    from . import state  # noqa: F401
