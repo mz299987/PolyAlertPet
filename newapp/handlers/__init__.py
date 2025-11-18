72eedeec2e29fc3baf7bf932a2654f6d8d9bcfcc
mz299987 <darmione1404@gmail.com>
2025-11-18 12:55:24 +0500

    Update __init__.py

--- newapp/handlers/__init__.py
+++ newapp/handlers/__init__.py
@@ -1,1 +1,21 @@
-# Handlers package
+"""
+Пакет обработчиков для бота Polymarket
+"""
+
+from .start import router as start_router
+from .wallets import router as wallets_router
+from .status import router as status_router
+from .analytics import router as analytics_router
+from .settings import router as settings_router
+from .betting import router as betting_router
+from .reports import router as reports_router
+
+__all__ = [
+    'start_router',
+    'wallets_router', 
+    'status_router',
+    'analytics_router',
+    'settings_router',
+    'betting_router',
+    'reports_router'
+]# Handlers package
